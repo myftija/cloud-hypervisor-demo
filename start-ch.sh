0ef00cd722a5a11ea56b27ee149678bf09d0541e
saadi <saadi.myftija@gmail.com>
2025-06-11 18:56:55 +0800

    Add script to start microVM with cloud-hypervisor
    Using the same kernel and settings as in firecraker-demo to enable comparing the two.

---
+++ start-ch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+
+SB_ID="${1:-0}"
+
+CH_BINARY="$PWD/resources/cloud-hypervisor"
+KERNEL="$PWD/resources/vmlinux"
+RO_DRIVE="$PWD/resources/rootfs.ext4"
+LOGFILE="$PWD/output/ch-sb${SB_ID}-log"
+
+TAP_DEV="ch-${SB_ID}-tap0"
+MASK="255.255.255.252"            # /30
+CH_IP=$(printf '169.254.%s.%s' $(((4*SB_ID+1)/256)) $(((4*SB_ID+1)%256)))
+TAP_IP=$(printf '169.254.%s.%s' $(((4*SB_ID+2)/256)) $(((4*SB_ID+2)%256)))
+MAC=$(printf '02:CC:00:00:%02X:%02X' $((SB_ID/256)) $((SB_ID%256)))
+
+CMDLINE="init=/sbin/boottime_init panic=1 pci=off nomodules reboot=k \
+tsc=reliable quiet i8042.nokbd i8042.noaux 8250.nr_uarts=0 ipv6.disable=1 \
+ip=${CH_IP}::${TAP_IP}:${MASK}::eth0:off root=/dev/vda ro"
+
+"${CH_BINARY}" \
+  --api-socket /tmp/ch-sb${SB_ID}.sock \
+  --kernel "${KERNEL}" \
+  --cmdline "${CMDLINE}" \
+  --disk path=${RO_DRIVE},readonly=true \
+  --net "tap=${TAP_DEV},mac=${MAC},ip=${CH_IP},mask=${MASK}" \
+  --cpus boot=1 \
+  --memory size=128M \
+  --serial file=${LOGFILE} \
+  --log-file ${LOGFILE} >> "$LOGFILE" &
