b4087a404531c0184abc217d9b28dabafe4b5251
saadi <saadi.myftija@gmail.com>
2025-06-18 15:46:17 +0200

    Measure network readiness after restore in a similar way to the boot approach

--- restore.sh
+++ restore.sh
@@ -22,6 +22,32 @@ curl --silent --show-error --unix-socket "${API_SOCKET}" -i \
 curl --silent --show-error --unix-socket "${API_SOCKET}" -i \
   -X PUT 'http://localhost/api/v1/vm.shutdown'
 
+restore_call_ts=$(date +%s.%N)
 "${CH_BINARY}" \
   --restore source_url=file://${SNAPSHOT_DIR} \
   --event-monitor path=${LOGFILE} >> "$LOGFILE" &
+
+curl --silent --show-error --unix-socket "${API_SOCKET}" -i \
+  -X PUT 'http://localhost/api/v1/vm.resume'
+
+# non-blocking network readiness check - ping
+{
+  max_attempts=600
+  attempt=0
+
+  while [ $attempt -lt $max_attempts ]; do
+    if ping -c 1 -W 1 "${CH_IP}" >/dev/null 2>&1; then
+      end_time=$(date +%s.%N)
+      time_diff_sec=$(echo "$end_time - $restore_call_ts" | bc -l)
+      time_diff_ms=$(printf "%.0f" $(echo "$time_diff_sec * 1000" | bc -l))
+      echo "RESTORE_TO_NETWORK_READY_PING_MS ${time_diff_ms}" >> "${LOGFILE}"
+      break
+    fi
+    attempt=$((attempt + 1))
+    sleep 0.05
+  done
+
+  if [ $attempt -ge $max_attempts ]; then
+    echo "RESTORE_TO_NETWORK_READY_PING_TIMEOUT" >> "${LOGFILE}"
+  fi
+} &
