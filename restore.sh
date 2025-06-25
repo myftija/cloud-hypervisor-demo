64e5452675dc526782f155ededf0769363e31d9f
saadi <saadi.myftija@gmail.com>
2025-06-25 15:16:39 +0200

    Add network readniness checks on tcp port 22 after vm restore

--- restore.sh
+++ restore.sh
@@ -63,3 +63,25 @@ curl --silent --show-error --unix-socket "${API_SOCKET}" -i \
     echo "RESTORE_TO_NETWORK_READY_PING_TIMEOUT" >> "${LOGFILE}"
   fi
 } &
+
+# non-blocking network readiness check - TCP port 22
+{
+  max_attempts=600
+  attempt=0
+
+  while [ $attempt -lt $max_attempts ]; do
+    if nc -z -w 1 "${CH_IP}" 22 >/dev/null 2>&1; then
+      end_time=$(date +%s.%N)
+      time_diff_sec=$(echo "$end_time - $restore_call_ts" | bc -l)
+      time_diff_ms=$(printf "%.0f" $(echo "$time_diff_sec * 1000" | bc -l))
+      echo "RESTORE_TO_NETWORK_READY_TCP22_MS ${time_diff_ms}" >> "${LOGFILE}"
+      break
+    fi
+    attempt=$((attempt + 1))
+    sleep 0.05
+  done
+
+  if [ $attempt -ge $max_attempts ]; then
+    echo "RESTORE_TO_NETWORK_READY_TCP22_TIMEOUT" >> "${LOGFILE}"
+  fi
+} &
