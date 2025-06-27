c5fb22eed31f7e0e9d6b486eb23dbf39dd574ab8
saadi <saadi.myftija@gmail.com>
2025-06-27 11:22:34 +0200

    Add instance type and vm size to the gnu plot titles

--- get-benchmarks.sh
+++ get-benchmarks.sh
@@ -25,47 +25,47 @@ gnuplot \
     -e "log_file='./$BENCHMARK_DIR/raw/boot.log';" \
     -e "output_file='./$BENCHMARK_DIR/plots/00_boot.png';" \
     -e "series_name='VM boot time';" \
-    -e "plot_title='Boot times'" \
+    -e "plot_title='Boot times | ${INSTANCE_TYPE} | ${VM_CPUS:-1} CPUs | ${VM_MEM:-128}MB'" \
     plot_distribution.script
 
 gnuplot \
     -e "log_file='./$BENCHMARK_DIR/raw/boot_to_network_ready_ping_probe.log';" \
     -e "output_file='./$BENCHMARK_DIR/plots/01_boot_to_network_ready_ping.png';" \
     -e "series_name='Boot to network ready (ping)';" \
-    -e "plot_title='Boot to Network Ready Times (Ping Probe)'" \
+    -e "plot_title='Boot to Network Ready Times (Ping Probe) | ${INSTANCE_TYPE} | ${VM_CPUS:-1} CPUs | ${VM_MEM:-128}MB'" \
     plot_distribution.script
 
 gnuplot \
     -e "log_file='./$BENCHMARK_DIR/raw/boot_to_network_ready_tcp22_probe.log';" \
     -e "output_file='./$BENCHMARK_DIR/plots/02_boot_to_network_ready_tcp22.png';" \
     -e "series_name='Boot to network ready (TCP22)';" \
-    -e "plot_title='Boot to Network Ready Times (TCP22 Probe)'" \
+    -e "plot_title='Boot to Network Ready Times (TCP22 Probe) | ${INSTANCE_TYPE} | ${VM_CPUS:-1} CPUs | ${VM_MEM:-128}MB'" \
     plot_distribution.script
 
 gnuplot \
     -e "log_file='./$BENCHMARK_DIR/raw/snapshot.log';" \
     -e "output_file='./$BENCHMARK_DIR/plots/03_snapshot.png';" \
     -e "series_name='VM snapshot time';" \
-    -e "plot_title='Snapshot Creation Times'" \
+    -e "plot_title='Snapshot Creation Times | ${INSTANCE_TYPE} | ${VM_CPUS:-1} CPUs | ${VM_MEM:-128}MB'" \
     plot_distribution.script
 
 gnuplot \
     -e "log_file='./$BENCHMARK_DIR/raw/restore.log';" \
     -e "output_file='./$BENCHMARK_DIR/plots/04_restore.png';" \
     -e "series_name='VM restore time';" \
-    -e "plot_title='Restore Times'" \
+    -e "plot_title='Restore Times | ${INSTANCE_TYPE} | ${VM_CPUS:-1} CPUs | ${VM_MEM:-128}MB'" \
     plot_distribution.script
 
 gnuplot \
     -e "log_file='./$BENCHMARK_DIR/raw/restore_to_network_ready_ping_probe.log';" \
     -e "output_file='./$BENCHMARK_DIR/plots/05_restore_to_network_ready_ping.png';" \
     -e "series_name='Restore to network ready (ping)';" \
-    -e "plot_title='Restore to Network Ready Times (Ping Probe)'" \
+    -e "plot_title='Restore to Network Ready Times (Ping Probe) | ${INSTANCE_TYPE} | ${VM_CPUS:-1} CPUs | ${VM_MEM:-128}MB'" \
     plot_distribution.script
 
 gnuplot \
     -e "log_file='./$BENCHMARK_DIR/raw/restore_to_network_ready_tcp22_probe.log';" \
     -e "output_file='./$BENCHMARK_DIR/plots/06_restore_to_network_ready_tcp22.png';" \
     -e "series_name='Restore to network ready (TCP22)';" \
-    -e "plot_title='Restore to Network Ready Times (TCP22 Probe)'" \
+    -e "plot_title='Restore to Network Ready Times (TCP22 Probe) | ${INSTANCE_TYPE} | ${VM_CPUS:-1} CPUs | ${VM_MEM:-128}MB'" \
     plot_distribution.script
